42402f53550c28ecb389686f703bcd1ac51605d8
KaterinaSmir <EkaterinaSmirnova.A@yandex.ru>
2022-04-04 00:16:00 +0300

    HW04_ver1.02

--- yatube/posts/tests/set_up_tests.py
+++ yatube/posts/tests/set_up_tests.py
@@ -75,9 +75,11 @@ class PostPagesLocators:
     POST_DETAIL = reverse('posts:post_detail', kwargs={'post_id': '4242'})
     ADD_COMMENT = reverse('posts:add_comment', kwargs={'post_id': '4242'})
     FOLLOW_USER_AUTHOR = reverse('posts:profile_follow',
-        kwargs={'username': 'auth2'})
+        kwargs={'username': 'auth2'}
+        )
     UNFOLLOW_USER_AUTHOR = reverse('posts:profile_unfollow',
-        kwargs={'username': 'auth2'})
+        kwargs={'username': 'auth2'}
+        )
     FOLLOW_INDEX = reverse('posts:follow_index')
     templates_url_names = (
         ('posts/index.html', '/'),
